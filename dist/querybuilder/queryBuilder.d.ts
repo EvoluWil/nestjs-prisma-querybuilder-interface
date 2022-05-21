d6e388593a4c42bee7d2c317b36b03efadf7d2ef
Willian Rodrigues <willian.rsilva1994@gmail.com>
2022-05-21 19:56:28 -0300

    Delete dist directory

--- dist/querybuilder/queryBuilder.d.ts
+++
@@ -1,2 +0,0 @@
-import { Query } from "./Query";
-export declare const queryBuilder: (query: Query) => string;
--- dist/querybuilder/queryBuilder.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.queryBuilder = void 0;
-const qs_1 = require("qs");
-const queryBuilder = (query) => {
-    return `?${qs_1.default.stringify(query)}`;
-};
-exports.queryBuilder = queryBuilder;
-//# sourceMappingURL=queryBuilder.js.map

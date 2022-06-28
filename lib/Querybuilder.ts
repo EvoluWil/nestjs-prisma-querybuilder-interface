2e7fd7ebbfd04eb1297ac9e8258c1d6beba61946
willian-rodrigues <willian.rsilva1994@gmail.com>
2022-06-28 15:13:49 -0300

    fix QueryString return type

--- lib/Querybuilder.ts
+++ lib/Querybuilder.ts
@@ -1,6 +1,6 @@
 import { stringify } from 'qs';
 import { Query } from './Query';
 
-export function QueryString(query: Query) {
+export function QueryString(query: Query): string {
   return stringify(query);
 }

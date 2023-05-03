76ec4643bd94bb24ebe801ec3b96290d9eb7ffca
Will <willian.rsilva1994@gmail.com>
2023-05-03 11:22:58 -0300

    fix multi array

--- lib/functions/FilterResolver.ts
+++ lib/functions/FilterResolver.ts
@@ -1,48 +1,49 @@
 import { FilterParse } from './FilterParse';
 
 export const FilterResolver = filters => {
-  return filters.map(filter => {
-    if (filter?.and) {
-      if (filter.and) {
-        return {
-          path: filter.path,
-          filter: FilterParse(filter.and, `and`)
-        };
+  return filters
+    .map(filter => {
+      if (filter?.and) {
+        if (filter.path) {
+          return {
+            path: filter.path,
+            filter: FilterParse(filter.and, `and`)
+          };
+        }
+
+        return FilterParse(filter.and, 'and');
       }
 
-      return FilterParse(filter.and, 'and');
-    }
-
-    if (filter?.or) {
-      if (filter.or) {
-        return {
-          path: filter.path,
-          filter: FilterParse(filter.or, `or`)
-        };
+      if (filter?.or) {
+        if (filter.path) {
+          return {
+            path: filter.path,
+            filter: FilterParse(filter.or, `or`)
+          };
+        }
+        return FilterParse(filter.or, 'or');
       }
 
-      return FilterParse(filter.or, 'or');
-    }
-
-    if (filter?.not) {
-      if (filter.path) {
-        return {
-          path: filter.path,
-          filter: FilterParse(filter.not, `not`)
-        };
+      if (filter?.not) {
+        if (filter.path) {
+          return {
+            path: filter.path,
+            filter: FilterParse(filter.not, `not`)
+          };
+        }
+        return FilterParse(filter.not, 'not');
       }
-      return FilterParse(filter.not, 'not');
-    }
 
-    if (filter?.filter) {
-      if (filter.path) {
-        return {
-          path: filter.path,
-          filter: FilterParse(filter.filter)
-        };
+      if (filter?.filter) {
+        if (filter.path) {
+          return {
+            path: filter.path,
+            filter: FilterParse(filter.filter)
+          };
+        }
       }
-    }
 
-    return FilterParse([filter])[0];
-  });
+      return FilterParse([filter]);
+    })
+    .flat();
 };
